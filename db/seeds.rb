fe9b60cef332e239365093c40c0bba63774a29ae
Javed <mjava1@yahoo.com>
2012-12-27 14:34:27 +1300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,9 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+
+puts 'CATEGORIES'
+YAML.load(ENV['CATEGORIES']).each do |category|
+  Category.find_or_create_by_name({ :name => category }, :without_protection => true)
+  puts 'category: ' << category
+end
