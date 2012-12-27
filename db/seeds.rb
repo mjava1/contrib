659a82e44c9b9b65898cd77bbba62cd94e8d372f
Javed <mjava1@yahoo.com>
2012-12-27 16:12:38 +1300

    seed data for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-#puts 'ROLES'
-#YAML.load(ENV['ROLES']).each do |role|
-#  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-#  puts 'role: ' << role
-#end
-#puts 'DEFAULT USERS'
-#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-#puts 'user: ' << user.name
-#user.add_role :admin
+puts 'ROLES'
+roles = ["admin", "user", "VIP"]
+roles.each do |role|
+  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  puts 'role: ' << role
+end
+puts 'DEFAULT USERS'
 
-#puts 'CATEGORIES'
-#YAML.load(ENV['CATEGORIES']).each do |category|
-#  Category.find_or_create_by_name({ :name => category }, :without_protection => true)
-#  puts 'category: ' << category
-#end
-#
+adminUserName = "admin"
+adminEmail = "admin@compapphq.com"
+adminPassword = "welcome"
+
+user = User.find_or_create_by_email :name => adminUserName, :email => adminEmail, :password => adminPassword, :password_confirmation => adminPassword
+puts 'user: ' << user.name
+user.add_role :admin
+
+puts 'CATEGORIES'
 categories = ["Free range", "Local", "Organic", "Vegetarian", "Eco-cleaners", "Recycling", "Composting", "Eco-packaging", "Sustainable Fish", "Food rescue", "BYO containers", "Fair trade"]
 categories.each do |category|
   Category.find_or_create_by_name({ :name => category }, :without_protection => true)
   puts 'category: ' << category
 end
-#Category.find_or_create_by_name({ :name => "Free range" }, :without_protection => true)
-#puts ""
