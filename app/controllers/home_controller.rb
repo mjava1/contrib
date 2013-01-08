1486aea0c81028d5b659d2b05074e876b12ecdee
Javed <mjava1@yahoo.com>
2013-01-08 14:47:02 +1300

    Updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @entries = Entry.all
-    @entries_by_category = Entry.all.group_by{ |entry| entry.category_id}
+    @entries_by_category = Entry.all(:include =>:category).group_by{ |entry| entry.category.name}
   end
 end
