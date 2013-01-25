c0cc8d0b68d36e1f249328ea170d46a7970a161f
Javed <mjava1@yahoo.com>
2013-01-25 16:18:57 +1300

    styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
 
     @entries_by_category = Entry.all(:include =>:category, :order =>"created_at DESC").group_by{ |entry| [entry.category.id, entry.category.name.tr(' ', '-').downcase]}
+    @show_comp_desc = true
+    @show_submit = true
   end
 end
